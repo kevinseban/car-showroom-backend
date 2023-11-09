83114b039495a57e4bcbe54233abaa4142f9750a
kevinseban <90603683+kevinseban@users.noreply.github.com>
2023-11-09 19:02:04 +0530

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -22,6 +22,6 @@ const userSchema = new mongoose.Schema({
   address: String,
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("Users", userSchema);
 
 module.exports = User;
