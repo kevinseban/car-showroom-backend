f05bcab6964ab1bcc0835f60824bdd5c1a0aae73
Adarsh S <112859491+Adarsh887@users.noreply.github.com>
2023-11-14 18:07:28 +0530

    Add files via upload

---
+++ controllers/bookingController.js
@@ -0,0 +1,17 @@
+const booking = require('../models/booking');
+
+const newBooking = async (req, res) => {
+    const { name, email, Username, Usercity, Userstate, Userpin, Dealername, Dealercity, Dealerstate, Carname, Carcolor, phone } = req.body;
+  
+    try {
+      const newbooking = new booking({ name, email, Username, Usercity, Userstate, Userpin, Dealername, Dealercity, Dealerstate, Carname, Carcolor, phone });
+      await newbooking.save();
+  
+      res.json({ message: "Booked successfully" });
+    } catch (error) {
+      console.error("Error booking:", error);
+      res.status(500).json({ error: "Server error" });
+    }
+  };
+
+module.exports = newBooking;
