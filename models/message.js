a02b58dfbeee715260380db21b5aebafb7b20313
kevinseban <90603683+kevinseban@users.noreply.github.com>
2023-11-14 15:21:55 +0530

    Update message.js

--- models/message.js
+++ models/message.js
@@ -3,10 +3,13 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://test:1234@cluster0.eu4qzfb.mongodb.net/carDB");
 
 const MessageSchema = new mongoose.Schema({
-    messName:String,
-    messEmail:String,
-    messPhone:String,
-    messMessage:String
+    messName: String,
+    messEmail: String,
+    messPhone: String,
+    messMessage: String,
+},
+{
+    timestamps: true
 })
 
 const Message = mongoose.model("messages", MessageSchema);
