0faee6cd11b8b3cb8133dd1bdc6591a83a09ea34
Adarsh S <112859491+Adarsh887@users.noreply.github.com>
2023-11-14 18:08:02 +0530

    Add files via upload

---
+++ models/booking.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://test:1234@cluster0.eu4qzfb.mongodb.net/carDB")
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch(() => {
+    console.log("Failed");
+  });
+
+  const bookingSchema = new mongoose.Schema({
+  
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    Username: {
+      type: String,
+      required: true,
+    },
+    Userstate: {
+      type: String,
+      required: true
+    },
+    phone: {
+      type: String,
+      required: true
+    },
+    Usercity: {
+      type: String,
+      required: true
+    },
+    Userpin: {
+      type: String,
+      required: true
+    },
+    Dealername: {
+      type: String,
+      required: true
+    },
+    Dealercity: {
+      type: String,
+      required: true
+    },
+    Dealerstate: {
+      type: String,
+      required: true
+    },
+    Carname: {
+      type: String,
+      required: true
+    },
+    Carcolor: {
+      type: String,
+      required: true
+    } 
+  });
+  
+  const booking = mongoose.model("booking",bookingSchema);
+
+module.exports = booking;
