ab017fb438f8a12d52f911697f8dc81d9fb1b0ab
kevinseban <90603683+kevinseban@users.noreply.github.com>
2023-11-15 13:14:35 +0530

    view booking in admin

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,17 +1,44 @@
 const booking = require('../models/Booking');
 
 const newBooking = async (req, res) => {
-    const { name, email, Username, Usercity, Userstate, Userpin, Dealername, Dealercity, Dealerstate, Carname, Carcolor, phone } = req.body;
-  
-    try {
-      const newbooking = new booking({ name, email, Username, Usercity, Userstate, Userpin, Dealername, Dealercity, Dealerstate, Carname, Carcolor, phone });
-      await newbooking.save();
-  
-      res.json({ message: "Booked successfully" });
-    } catch (error) {
-      console.error("Error booking:", error);
-      res.status(500).json({ error: "Server error" });
-    }
-  };
+  const { name, email, Username, Usercity, Userstate, Userpin, Dealername, Dealercity, Dealerstate, Carname, Carcolor, phone } = req.body;
 
-module.exports = {newBooking};
+  try {
+    const newbooking = new booking({ name, email, Username, Usercity, Userstate, Userpin, Dealername, Dealercity, Dealerstate, Carname, Carcolor, phone });
+    await newbooking.save();
+
+    res.json({ message: "Booked successfully" });
+  } catch (error) {
+    console.error("Error booking:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+const getBookings = async (req, res) => {
+  try {
+    const bookings = await booking.find();
+    res.json(bookings);
+  } catch (error) {
+    console.error("Error fetching bookings:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+const deleteBooking = async (req, res) => {
+  const bookingId = req.params.id;
+
+  try {
+    const deletedBooking = await booking.findByIdAndDelete(bookingId);
+
+    if (!deletedBooking) {
+      return res.status(404).json({ error: 'Booking not found' });
+    }
+
+    res.json({ message: 'Booking deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting booking:', error);
+    res.status(500).json({ error: 'Server error' });
+  }
+};
+
+module.exports = { newBooking, getBookings, deleteBooking };
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const bookingController = require('../controllers/bookingController');
 
-router.post('/newBooking',bookingController.newBooking);
+router.post('/newBooking', bookingController.newBooking);
+router.get('/getBookings', bookingController.getBookings);
+router.delete('/deleteBooking/:id', bookingController.deleteBooking);
 
 module.exports = router;
